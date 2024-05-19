bf347732fbc4b91cd6d06a916a72f712bb3c7f19
Azriel Pazarudin <sapuinjukemak@gmail.com>
2024-05-19 15:43:00 +0700

    update-schema-ddl

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,23 +1,22 @@
-CREATE DATABASE IF NOT EXISTS `park` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+CREATE DATABASE `park` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 -- park.`user` definition
 
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `image` blob,
   `jurusan` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
-  `name_image` varchar(255) DEFAULT NULL,
   `nim` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `plat` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 -- park.tampungan definition
 
 CREATE TABLE `tampungan` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `image_name` varchar(255) DEFAULT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `image` blob,
   `password` varchar(255) DEFAULT NULL,
   `username` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
